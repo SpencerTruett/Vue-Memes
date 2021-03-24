1c150396850ae428411a54785d0386fb7bd1e957
Spencer Truett <srtruett1993@gmail.com>
2021-03-24 10:15:44 -0500

    hooked up basic routing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
-<template></template>
+<template>
+  <v-app>
+    <v-app-bar app color="teal darken-2" dark></v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
-
-<style>
-</style>
