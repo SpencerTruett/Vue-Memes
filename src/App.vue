7cdf27c96aac58e863a0347f5860e05f1f64469d
Spencer Truett <srtruett1993@gmail.com>
2021-03-24 11:05:38 -0500

    added v-btn and css styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <v-app>
-    <v-app-bar app color="teal darken-2" dark></v-app-bar>
+    <v-app-bar app color="teal darken-2" dark>
+      <h1>Vue Meme Forum</h1>
+      <v-spacer></v-spacer>
+      <router-link to="/create">
+        <v-btn text>Create</v-btn>
+      </router-link>|
+      <router-link to="/feed">
+        <v-btn text>Memes</v-btn>
+      </router-link>
+    </v-app-bar>
 
     <v-main>
       <router-view></router-view>
@@ -11,3 +20,12 @@
 <script>
 export default {};
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+.router-link-active .v-btn {
+  color: rgb(255, 200, 100);
+}
+</style>
