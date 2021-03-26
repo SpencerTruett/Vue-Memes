6b303ee0b6634cdb4ba850ffe05dbfefeb6aacb1
Spencer Truett <srtruett1993@gmail.com>
2021-03-26 11:06:29 -0500

    cleaned up my search and added a query string

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,12 +1,23 @@
 <template>
   <v-container>
-    <form @submit.prevent="addSearch">
-      <input type="text" v-model="inputVal" placeholder="Search" />
-    </form>
+    <v-row justify="center" class="mt-5">
+      <v-col :sm="12" :md="6">
+        <form @submit.prevent="addSearch">
+          <v-text-field
+            prepend-inner-icon="mdi-magnify"
+            color="teal darken-2"
+            rounded
+            outlined
+            v-model="inputVal"
+            label="Search"
+          />
+        </form>
+      </v-col>
+    </v-row>
     <div v-for="meme in displayedMemes" :key="meme.id" class="py-5">
       <router-link :to="`/meme/${meme.id}`">
         <meme
-          class="mx-auto mt-6"
+          class="mx-auto"
           :top="meme.topText"
           :bottom="meme.bottomText"
           :imageURL="meme.imageURL"
@@ -32,14 +43,21 @@ export default {
   methods: {
     addSearch() {
       this.searchTerm = this.inputVal;
+      this.$router.push({
+        path: "/feed",
+        query: { q: this.searchTerm }
+      });
     }
   },
   mounted() {
-    db.collection("memes").onSnapshot((snap) => {
-      const memes = snap.docs.map((doc) => {
+    this.inputVal = this.$route.query.q;
+    this.searchTerm = this.$route.query.q;
+
+    db.collection("memes").onSnapshot(snap => {
+      const memes = snap.docs.map(doc => {
         return {
           ...doc.data(),
-          id: doc.id,
+          id: doc.id
         };
       });
       this.memes = memes;
